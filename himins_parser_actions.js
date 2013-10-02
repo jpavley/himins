fd957542c4588c49194bbce80a50cf8fac064576
John Pavley <jpavley@gmail.com>
2013-10-02 10:15:39 -0400

    splitting parser in to sub mods

--- himins_parser_actions.js
+++ himins_parser_actions.js
@@ -1,2 +1,59 @@
 // himins_parser_actions.js
 // execute actions based on user input
+
+var WELCOME_MESSAGE = 0;
+
+var welcomeAction = function (client, lingo) {
+  // action
+  client.write(renderMessageForDisplay(client, WELCOME_MESSAGE, lingo));
+  // post action
+  client.write(display.prompt);
+};
+
+var helpAction = function (client, lingo) {
+  
+};
+
+var aboutAction = function (client, lingo) {
+  
+};
+
+var langugeAction = function (client, lingo) {
+  
+};
+
+var newsAction = function (client, lingo) {
+  
+};
+
+var quitAction = function (client, lingo) {
+  
+};
+
+var renameAction = function (client, lingo) {
+  
+};
+
+var startAction = function (client, lingo) {
+  
+};
+
+var timeAction = function (client, lingo) {
+  
+};
+
+var englishAction = function (client, lingo) {
+  
+};
+
+var spanishAction = function (client, lingo) {
+  
+};
+
+var sayAction = function (client, lingo) {
+  
+};
+
+var tellAction = function (client, lingo) {
+  
+};
