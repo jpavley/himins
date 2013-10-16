d651461defe50209bc11e015673fbe477ae4360c
John Pavley <jpavley@gmail.com>
2013-10-16 09:39:39 -0400

    starting on the room implementation. Acutal game stuff!

---
+++ himins_room.js
@@ -0,0 +1,15 @@
+// # himins_room.js
+// ## Represents a room in a zone in the game world.
+// Rooms generally have entrances and exits, items, puzzels, and mobs.
+
+var user = require('./himins_user'),
+    display = require('./himins_client');
+    
+var discriptionsFileName = "",
+    discriptions = [],
+    hotSpots = [],
+    items = [],
+    puzzels = [];
+
+// #init()
+    
