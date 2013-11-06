2160506142e9a2374937b9f1100dba2c39821147
John Pavley <jpavley@gmail.com>
2013-11-06 09:04:29 -0500

    added a fist full of accessors

--- himins_player.js
+++ himins_player.js
@@ -29,6 +29,58 @@ var loadPlayer = function (playerFileName) {
 };
 module.exports.loadPlayer = loadPlayer;
 
+//# getPlayerInventoryNames
+// Returns a string with the names of the items in the player inventory
+var getPlayerInventoryNames = function () {
+	var namesList = [],
+		result = '';
+
+	for (var i = playerObject.inventory.length - 1; i >= 0; i--) {
+		nameList.push(playerObject.inventory[i].name);
+	};
+
+	result = namesList.toString();
+	result = result.replace(/,/g, ", ");
+	return result;
+};
+module.exports.getPlayerInventoryNames = getPlayerInventoryNames;
+
+// # getPlayerLocation()
+var getPlayerLocation = function () {
+	return playerObject.location;
+};
+module.exports.getPlayerLocation = getPlayerLocation;
+
+// # setPlayerLocation(location)
+var setPlayerLocation = function (location) {
+	playerObject.location = location;
+};
+module.exports.setPlayerLocation = setPlayerLocation;
+
+// # getPlayerRoom()
+var getPlayerRoom = function () {
+	return playerObject.room;
+};
+module.exports.getPlayerRoom = getPlayerRoom;
+
+// # setPlayerRoom(room)
+var setPlayerRoom = function (room) {
+	playerObject.room = room;
+};
+module.exports.setPlayerRoom = setPlayerRoom;
+
+// # getPlayerHealth()
+var getPlayerHealth = function () {
+	return playerObject.health;
+};
+module.exports.getPlayerHealth = getPlayerHealth;
+
+// # setPlayerHealth(hitPoints)
+var setPlayerHealth = function (hitPoints) {
+	playerObject.health = hitPoints;
+};
+module.exports.getPlayerHealth = getPlayerHealth;
+
 // # main entry point
 // For testing purposes you can run this file directly with "node himins_player.js". The test logic expects a file named "himins_player.json" with the defination of a player object!
 
