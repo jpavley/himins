24a3d205a26d8539e345908d9f06c44722e510fa
John Pavley <jpavley@gmail.com>
2013-11-12 18:20:29 -0500

    JSLint fixes

--- himins_player.js
+++ himins_player.js
@@ -2,111 +2,114 @@
 // Manages a player object from a himins player JSON file.
 
 // ## includes
-var fs = require('fs'),
-		commands = require('./himins_commands.js');
+var
+  fs = require('fs'),
+  commands = require('./himins_commands.js');
 
 // ## module vars
 var playerObject = {};
 
 //# init()
 var init = function () {
-	console.log('*** (5) himins_player.js init()');
+  console.log('*** (5) himins_player.js init()');
 
-	commands.addCommand({ name: playerObject.name, 
-												description: playerObject.description,
-												action: '!NO_ACTION',
-												kind: 'player' });
+  commands.addCommand({ name: playerObject.name,
+                        description: playerObject.description,
+                        action: '!NO_ACTION',
+                        kind: 'player' });
 
 };
 module.exports.init = init;
 
 //# loadPlayer(playerFileName)
 var loadPlayer = function (playerFileName) {
-	console.log('*** (4) himins_player.js playerRoom(%s)', playerFileName);
+  console.log('*** (4) himins_player.js playerRoom(%s)', playerFileName);
 
-	fs.readFile(playerFileName, 'utf8', function (err, data) {
-		if(err) {
-			console.log(err);
-		} else {
-			playerObject = JSON.parse(data);
-			//console.log(playerObject);
-			init();
-		}
-	});
+  fs.readFile(playerFileName, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      playerObject = JSON.parse(data);
+      //console.log(playerObject);
+      init();
+    }
+  });
 };
 module.exports.loadPlayer = loadPlayer;
 
 //# getPlayerInventoryNames
 // Returns a string with the names of the items in the player inventory
 var getPlayerInventoryNames = function () {
-	var namesList = [],
-		result = '';
+  var
+    namesList = [],
+    result = '',
+    i;
 
-	for (var i = playerObject.inventory.length - 1; i >= 0; i--) {
-		nameList.push(playerObject.inventory[i].name);
-	};
+  for (i = playerObject.inventory.length - 1; i >= 0; i--) {
+    namesList.push(playerObject.inventory[i].name);
+  }
 
-	result = namesList.toString();
-	result = result.replace(/,/g, ", ");
-	return result;
+  result = namesList.toString();
+  result = result.replace(/,/g, ", ");
+  return result;
 };
 module.exports.getPlayerInventoryNames = getPlayerInventoryNames;
 
 // # getPlayerLocation()
 var getPlayerLocation = function () {
-	return playerObject.location;
+  return playerObject.location;
 };
 module.exports.getPlayerLocation = getPlayerLocation;
 
 // # setPlayerLocation(location)
 var setPlayerLocation = function (location) {
-	playerObject.location = location;
+  playerObject.location = location;
 };
 module.exports.setPlayerLocation = setPlayerLocation;
 
 // # getPlayerRoom()
 var getPlayerRoom = function () {
-	return playerObject.room;
+  return playerObject.room;
 };
 module.exports.getPlayerRoom = getPlayerRoom;
 
 // # setPlayerRoom(room)
 var setPlayerRoom = function (room) {
-	playerObject.room = room;
+  playerObject.room = room;
 };
 module.exports.setPlayerRoom = setPlayerRoom;
 
 // # getPlayerHealth()
 var getPlayerHealth = function () {
-	return playerObject.health;
+  return playerObject.health;
 };
 module.exports.getPlayerHealth = getPlayerHealth;
 
 // # setPlayerHealth(hitPoints)
 var setPlayerHealth = function (hitPoints) {
-	playerObject.health = hitPoints;
+  playerObject.health = hitPoints;
 };
 module.exports.getPlayerHealth = getPlayerHealth;
 
 // # getPlayerName()
 var getPlayerName = function () {
-	return playerObject.name;
+  return playerObject.name;
 };
 module.exports.getPlayerName = getPlayerName;
 
 // # setPlayerName(name)
 var setPlayerName = function (name) {
-	// remove the player command with the old name...
-	commands.removeCommandByName(playerObject.name);
+  // remove the player command with the old name...
+  commands.removeCommandByName(playerObject.name);
 
-	// update the name of the player
-	playerObject.name = name;
+  // update the name of the player
+  playerObject.name = name;
 
-	// ...add a player command with new name
-	commands.addCommand({ name: playerObject.name, 
-												description: playerObject.description,
-												action: '!NO_ACTION',
-												kind: 'player' });
+  // ...add a player command with new name
+  commands.addCommand({ name: playerObject.name,
+                        description: playerObject.description,
+                        action: '!NO_ACTION',
+                        kind: 'player' });
 
 };
 module.exports.setPlayerName = setPlayerName;
