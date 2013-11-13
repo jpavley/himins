dafdbf154566a07ad6c3940777c23422484fba55
John Pavley <jpavley@gmail.com>
2013-11-12 22:04:48 -0500

    JSLint fixes

--- himins_string_utils.js
+++ himins_string_utils.js
@@ -6,29 +6,32 @@ var addedFlag = false;
 
 // # addStringRepeat()
 var addStringRepeat = function () {
-	String.prototype.repeat = function (count) {
-		return new Array(count + 1).join(this);
-	};
+  String.prototype.repeat = function (count) {
+    var result = [];
+    result = result.join(this);
+    return result;
+    //return new Array(count + 1).join(this);
+  };
 };
 
 // # addStringUnformattedLength()
 // Calcs the length of a string without markdown
 var addStringUnformattedLength = function () {
-	String.prototype.unformattedLength = function () {
-		var result = this;
-		 // remove underscores, astericks, and seperators!
-		result = this.split('_').join('');
-		result = this.split('*').join('');
-		return result.length;
-	};
+  String.prototype.unformattedLength = function () {
+    var result = this;
+     // remove underscores, astericks, and seperators!
+    result = this.split('_').join('');
+    result = this.split('*').join('');
+    return result.length;
+  };
 };
 
 // # init()
 var init = function () {
-	if (!addedFlag) {
-		addStringRepeat();
-		addStringUnformattedLength();
-		addedFlag = true;
-	};
+  if (!addedFlag) {
+    addStringRepeat();
+    addStringUnformattedLength();
+    addedFlag = true;
+  }
 };
 module.exports.init = init;
