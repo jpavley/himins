ed1417e07edc2338e60c9f4943747d89a513bb21
John Pavley <jpavley@gmail.com>
2013-11-12 22:01:10 -0500

    JSLint fixes

--- himins_room.js
+++ himins_room.js
@@ -2,130 +2,135 @@
 // Manages a room object from a himins room JSON file.
 
 // ## includes
-var fs = require('fs'),
-		commands = require('./himins_commands.js'),
-		game = require('./himins_game.js'),
-		player = require('./himins_player.js');
+var
+  fs = require('fs'),
+  commands = require('./himins_commands.js'),
+  game = require('./himins_game.js'),
+  player = require('./himins_player.js');
 
 // ## module vars
 var roomObject = {};
 
-//# init()
-var init = function () {
-	console.log('*** (3) himins_room.js init()');
+// # getSectionByName(name)
+var getSectionByName = function (name) {
+  //console.log("*** himins_room.js getSectionByName(%s)", name);
+  var result = {}, i;
 
-	// player
-	player.loadPlayer(roomObject.defaultPlayer, false);
-	player.setPlayerLocation(roomObject.spawnSection);
-
-
-	// commands
-	loadItemCommands(player.getPlayerLocation());
-	loadNavigationCommands();
+  for (i = roomObject.sections.length - 1; i >= 0; i--) {
+    if (roomObject.sections[i].name === name) {
+      result = roomObject.sections[i];
+      break;
+    }
+  }
+  return result;
 };
-module.exports.init = init;
-
-//# loadRoom(roomFileName)
-var loadRoom = function (roomFileName) {
-	console.log('*** (3) himins_room.js loadRoom(%s)', roomFileName);
-
-	fs.readFile(roomFileName, 'utf8', function (err, data) {
-		if(err) {
-			console.log(err);
-		} else {
-			roomObject = JSON.parse(data);
-			//console.log(roomObject.name);
-			init();
-			welcome();
-			//processUserInput();
-		}
-	});
-};
-module.exports.loadRoom = loadRoom;
 
 // # loadItemCommands(sectionName)
 var loadItemCommands = function (sectionName) {
-	// console.log('*** himins_room.js loadItemCommands(%s)', sectionName);
+  // console.log('*** himins_room.js loadItemCommands(%s)', sectionName);
 
-	var sectionObject = getSectionByName(sectionName);
+  var sectionObject = getSectionByName(sectionName), i;
 
-	// remove old section commands
-	commands.removeCommandsByKind('item');
+  // remove old section commands
+  commands.removeCommandsByKind('item');
 
-	if (sectionObject.items) {
-		// some items in section, load commands
-		for (var i = sectionObject.items.length - 1; i >= 0; i--) {
-			commands.addCommand({ name: sectionObject.items[i].name, 
-														description: sectionObject.items[i].description,
-														action: '!ADD_TO_INVENTORY',
-														kind: 'item' });
-		}
-	}
+  if (sectionObject.items) {
+    // some items in section, load commands
+    for (i = sectionObject.items.length - 1; i >= 0; i--) {
+      commands.addCommand({ name: sectionObject.items[i].name,
+                            description: sectionObject.items[i].description,
+                            action: '!ADD_TO_INVENTORY',
+                            kind: 'item' });
+    }
+  }
 };
 
 // # loadNavigationCommands()
 var loadNavigationCommands = function () {
-	// remove all navigation commands
-	commands.removeCommandsByKind('navigation');
+  var i;
+  // remove all navigation commands
+  commands.removeCommandsByKind('navigation');
 
-	for (var i = roomObject.sections.length - 1; i >= 0; i--) {
-		commands.addCommand({ name: roomObject.sections[i].name, 
-													description: roomObject.sections[i].description,
-													action: '!SET_LOCATION',
-													kind: 'navigation' });
-	}
-};
-
-// # getSectionByName(name)
-var getSectionByName = function (name) {
-	//console.log("*** himins_room.js getSectionByName(%s)", name);
-	var result = {};
-
-	for (var i = roomObject.sections.length - 1; i >= 0; i--) {
-		if (roomObject.sections[i].name === name) {
-			result = roomObject.sections[i];
-			break;
-		}
-	}
-	return result;
-};
-
-// # getItemByName(sectionName, itemName)
-var getItemByName = function (sectionName, itemName) {
-	var result = {},
-			sectionObject = getSectionByName(sectionName);
-
-	if (sectionObject) {
-		for (var i = sectionObject.items.length - 1; i >= 0; i--) {
-			if (sectionObject.items[i].name === itemName) {
-				result = sectionObject.items[i];
-			}
-		}
-	}
+  for (i = roomObject.sections.length - 1; i >= 0; i--) {
+    commands.addCommand({ name: roomObject.sections[i].name,
+                          description: roomObject.sections[i].description,
+                          action: '!SET_LOCATION',
+                          kind: 'navigation' });
+  }
 };
 
 // # welcome()
 // called when a player spawns or enters a room
 var welcome = function () {
-	var commandMap = commands.getCommandMap();
+  var commandMap = commands.getCommandMap();
 
-	console.log();
-	commands.doGameCommand(null, '', commandMap['look']);
-	console.log();
+  console.log();
+  commands.doGameCommand(null, '', commandMap.look);
+  console.log();
+};
+
+//# init()
+var init = function () {
+  console.log('*** (3) himins_room.js init()');
+
+  // player
+  player.loadPlayer(roomObject.defaultPlayer, false);
+  player.setPlayerLocation(roomObject.spawnSection);
+
+
+  // commands
+  loadItemCommands(player.getPlayerLocation());
+  loadNavigationCommands();
+};
+module.exports.init = init;
+
+//# loadRoom(roomFileName)
+var loadRoom = function (roomFileName) {
+  console.log('*** (3) himins_room.js loadRoom(%s)', roomFileName);
+
+  fs.readFile(roomFileName, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      roomObject = JSON.parse(data);
+      //console.log(roomObject.name);
+      init();
+      welcome();
+      //processUserInput();
+    }
+  });
+};
+module.exports.loadRoom = loadRoom;
+
+// # getItemByName(sectionName, itemName)
+var getItemByName = function (sectionName, itemName) {
+  var
+    result = {},
+    sectionObject = getSectionByName(sectionName),
+    i;
+
+  if (sectionObject) {
+    for (i = sectionObject.items.length - 1; i >= 0; i--) {
+      if (sectionObject.items[i].name === itemName) {
+        result = sectionObject.items[i];
+      }
+    }
+  }
 };
 
 // # getRoomDescription()
 var getRoomDescription = function () {
-	return roomObject.description;
+  return roomObject.description;
 };
 module.exports.getRoomDescription = getRoomDescription;
 
 // # getSectionDescription()
 var getSectionDescription = function () {
-	var section = getSectionByName(player.getPlayerLocation()),
-			result = section.description;
+  var
+    section = getSectionByName(player.getPlayerLocation()),
+    result = section.description;
 
-	return result;
+  return result;
 };
 module.exports.getSectionDescription = getSectionDescription;
 
