102a4735f3a97f8f836e445edd3b356fb242b137
John Pavley <john@pavley.com>
2015-06-19 19:40:42 -0400

    removed cruft

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,5 @@
 module.exports = function(grunt) {
 
-	var banner = '/*n<%= pkg.name %> <%= pkg.version %>';
-	banner += '- <%= pkg.description %>n<%= pkg.repository.url %>n';
-	banner += 'Built on <%= grunt.template.today("yyyy-mm-dd") %>n*/n';
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
@@ -13,7 +9,8 @@ module.exports = function(grunt) {
 				'himins_js/*.js',
 				'experiments/*.js'
 			],
-		}
+		},
+
 	});
 
 	// plub-in tasks
