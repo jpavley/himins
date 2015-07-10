db89800296f38694105960584218516be0d7f328
John Pavley <john@pavley.com>
2015-07-09 22:46:55 -0400

    refactoring format module

--- himins_js/himins_format.js
+++ himins_js/himins_format.js
@@ -1,102 +1,57 @@
 /**
  * @fileOverview formats text and resolves functions for display
+ * User passes in a context and a template and format returns a rendered string
  * @module
  * @requires linewrap
  * @requires underscore
- * @requires ./himins_js/himins_game
- * @requires ./himins_js/himins_room
- * @requires ./himins_js/himins_player
- * @requires ./himins_js/himins_commands
+ * @requires mustache
+ * @requires colors
  */
 
 
-// ## includes
+// includes
 var
-  // ### 3rd party modules
-  _ = require('underscore'),
   linewrap = require('linewrap'),
+  mustache = require('mustache'),
+  colors = require('colors');
 
-  // ### Himins modules
-  room = require('./himins_room.js'),
-  commands = require('./himins_commands.js'),
-  game = require('./himins_game.js'),
-  player = require('./himins_player.js');
-
-// ## consts
+// constants
 var
-  BOLD_RED_ESC = '\u001b[1;31m',
-  BOLD_GREEN_ESC = '\u001b[1;32m',
-  UL_BLUE_ESC = '\u001b[4;34m',
-  NORMAL_ESC = '\u001b[0m',
-  NL = '\n';
+  defaultTheme = {
+    silly: 'rainbow',
+    input: 'gray',
+    verbose: 'cyan',
+    prompt: 'magenta',
+    info: 'green',
+    data: 'inverse',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  };
 
-module.exports.BOLD_RED_ESC = BOLD_RED_ESC;
-module.exports.NORMAL_ESC = NORMAL_ESC;
-
-var renderFormatCodes = function(text) {
-  var result = text;
-
-  result = result.replace(/^_/g, ' ' + BOLD_RED_ESC); // match _ at SOL
-  result = result.replace(/ _/g, ' ' + BOLD_RED_ESC);
-  result = result.replace(/_ /g, NORMAL_ESC + ' ');
-  result = result.replace(/_,/g, NORMAL_ESC + ',');
-  result = result.replace(/_\./g, NORMAL_ESC + '.');
-  result = result.replace(/_;/g, NORMAL_ESC + ';');
-  result = result.replace(/_:/g, NORMAL_ESC + ':');
-  result = result.replace(/_-/g, NORMAL_ESC + '-');
-  //result = result.replace(/_\?/g, NORMAL_ESC + '?');
-  result = result.replace(/_$/g, NORMAL_ESC); // match _ at EOL
-
-  result = result.replace(/^\*/g, ' ' + BOLD_GREEN_ESC); // match * at SOL
-  result = result.replace(/ \*/g, ' ' + BOLD_GREEN_ESC);
-  result = result.replace(/\* /g, NORMAL_ESC + ' ');
-  result = result.replace(/\*,/g, NORMAL_ESC + ',');
-  result = result.replace(/\*\./g, NORMAL_ESC + '.');
-  result = result.replace(/\*;/g, NORMAL_ESC + ';');
-  result = result.replace(/\*:/g, NORMAL_ESC + ':');
-  result = result.replace(/\*-/g, NORMAL_ESC + '-');
-  //result = result.replace(/\*\?/g, NORMAL_ESC + '?');
-  result = result.replace(/\*$/g, NORMAL_ESC); // match * at EOL
-
-  return result;
+var getDefaultTheme = function() {
+  return defaultTheme;
 };
 
-var resolveFunctions = function(client, text) {
-  //console.log('*** himins_format.js resolveFunctions(%s)', text);
-  var
-    result = text,
-    playerObject = client.player,
-    gameObject = playerObject.game,
-    roomObject = game.getRoomByName(gameObject, playerObject.roomName),
-    sectionObject = {};
-  
-  result = result.replace(/!GAME_NAME/g, client.player.game.name);
-  result = result.replace(/!PLAYER_NAME/g, client.player.name);
-  result = result.replace(/!COMMAND_NAMES/g, commands.getCommandNames(playerObject.commands));
-  result = result.replace(/!PLAYER_INVENTORY/g, player.getInventoryNames(playerObject));
-  result = result.replace(/!PLAYER_LOCATION/g, playerObject.sectionName);
-  result = result.replace(/!ROOM_NAME/g, playerObject.roomName);
-  result = result.replace(/!PLAYER_HEALTH/g, playerObject.health);
-  result = result.replace(/!MOVING_VERB/g, _.sample(gameObject.movementVerbs));
-  result = result.replace(/!MOVING_ADVERB/g, _.sample(gameObject.movementAdverbs));
-  result = result.replace(/!GAME_ATMOSPHERE/g, _.sample(gameObject.atmosphereAdjectives));
-
-  if (roomObject) {
-    sectionObject = room.getSectionByName(roomObject, playerObject.sectionName);
-    result = result.replace(/!ROOM_DESCRIPTION/g, roomObject.description);
-  }
-
-  if (sectionObject) {
-    result = result.replace(/!SECTION_DESCRIPTION/g, sectionObject.description);
-  }
-  return result;
+var setDefaultTheme = function(newTheme) {
+  defaultTheme = newTheme;
 };
 
-// # formatText(client, text, indent, columnWidth)
-// Transforms text (with basic markdown syntax into ASCII TTY)
-// and wraps it to fit column specified by columnWidth.
-// Words that start with an exclamation point (!WORD) are treated as function identifiers
-var formatText = function(client, text, indent, columnWidth) {
+
+/** 
+ * Transforms context and template into text.
+ * Waps text to fit column specified by indent and columnWidth.
+ * Styles text according to style
+ * Returns the rendered text.
+ * @param {String} context
+ * @param {String} template
+ * @param {Number} indent
+ * @param {Number} width
+ * @param {String} style
+ * @returns {String} text
+ */
+var formatText = function(context, template, indent, width, style) {
   //console.log('*** himins_format.js formatText(%s, %s, %d, %d)', client.name, text, indent, columnWidth);
 
   var
